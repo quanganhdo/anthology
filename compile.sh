71ada27b735dbd48a45cab74ce024909448cd869
Anh Do <hello@anh.do>
2019-10-13 11:30:15 -0400

    Add newline if necessary + Change port to 8080

--- compile.sh
+++ compile.sh
@@ -3,6 +3,9 @@ if [[ $# -eq 0 ]]; then
 	exit 1
 fi
 
+# Add newline if necessary
+sed -i -e '$a\' "$1"
+
 # One temp dir is enough
 if [ -d 'tmp' ]; then
 	rm -r tmp
@@ -16,7 +19,7 @@ while IFS= read -r line; do
 	counter=$[counter+1]
 	
 	# Parse
-	curl --header "x-api-key: REPLACE_THIS_WITH_YOUR_API_KEY" "https://mercury.postlight.com/parser?url=$line" > tmp/__INTERNAL__/$counter.json
+  mercury-parser "$line" > tmp/__INTERNAL__/$counter.json
 	jq -r '.content' tmp/__INTERNAL__/$counter.json > tmp/__INTERNAL__/$counter.html
 	
 	# Cache
@@ -24,7 +27,7 @@ while IFS= read -r line; do
 	mkdir "tmp/$counter"
 	
 	# Force ipv4
-	wget -4 -e robots=off --page-requisites --span-hosts --convert-links --adjust-extension --no-directories --directory-prefix="tmp/$counter" --accept-regex="(jpe?g|gif|png|html?)" "http://localhost:8000/tmp/__INTERNAL__/$counter.html"
+	wget -4 -e robots=off --page-requisites --span-hosts --convert-links --adjust-extension --no-directories --directory-prefix="tmp/$counter" --accept-regex="(jpe?g|gif|png|html?)" "http://localhost:8080/tmp/__INTERNAL__/$counter.html"
 	
 	# So that Calibre won't choke
 	sed -i'.bak' "1s/^/<html><head><h1>$title<\/h1><\/head><body>/" "tmp/$counter/$counter.html"
@@ -37,6 +40,6 @@ done < "$1"
 echo "</ol></body></html>" >> tmp/index.html
 
 # Finalize
-output=$(basename $1)
+output=$(basename "$1")
 ebook-convert tmp/index.html "$output.epub" --smarten-punctuation --chapter-mark both --change-justification left --title "$output" --no-chapters-in-toc --authors "Multiple Authors" 
 # ebook-convert "$output.epub" "$output.mobi" --output-profile kindle_voyage
